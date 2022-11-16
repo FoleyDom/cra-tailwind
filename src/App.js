75ad9aacf2e76a051cd5ce330a781493a7f1a319
Dominique Foley <dfoleydev@gmail.com>
2022-11-16 12:11:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Hero from './components/Hero'
+import Product from './components/Product'
+
+function App() {
+	return (
+		<>
+			<Hero />
+			<Product />
+		</>
+	)
+}
+
+export default App
