f526509a9edace5a27369c91857e390248aba2d8
Dominique Foley <dfoleydev@gmail.com>
2022-11-16 20:28:32 -0500

    Creating components to get layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Hero from './components/Hero'
 import Product from './components/Product'
+import Nav from './components/Nav'
 
 function App() {
     return (
         <>
+            <Nav />
             <Hero />
             <Product />
         </>
